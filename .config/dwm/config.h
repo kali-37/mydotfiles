f2dc7494f6672d357edb7abdbbbb5469828e7051
Subash1237 <rimalsubash77@gmail.com>
2024-02-07 11:43:31 +0545

    so just added

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -14,7 +14,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
+static const int vertpad            = 2;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrains Mono:size=10" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
